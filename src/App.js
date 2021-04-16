a09fde00adf9660ffca25cdc19f6fb73f5aa988c
ayushgupta-repo <19ce037@charusat.edu.in>
2021-04-17 02:07:23 +0530

    Covid Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  useEffect (() => {
+
+    const getCountriesData = async () => {
+      await fetch ("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => (
+          {
+            name: country.country, // United States, United Kingdom
+            value: country.countryInfo.iso2, // UK, USA, FR
+          }));
+
+          setCountries(countries);
+      });
+    };
+
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    console.log('YOOOOO', countryCode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app__dropdown">
+          <Select
+            variant="outlined" value={country} onChange={onCountryChange}
+          >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }

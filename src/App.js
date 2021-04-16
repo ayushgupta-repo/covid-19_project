5edefe11f11ec244e19044bc617fa61997aefaf1
ayushgupta-repo <19ce037@charusat.edu.in>
2021-04-17 03:18:02 +0530

    InfoBox__MapSection__LeftRight

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import {
   MenuItem,
   FormControl,
   Select,
+  Card,
+  CardContent
 } from "@material-ui/core";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
 import './App.css';
 
 function App() {
@@ -29,39 +33,51 @@ function App() {
     getCountriesData();
   }, []);
 
-  const onCountryChange = async (event) => {
+  const onCountryChange =(event) => {
     const countryCode = event.target.value;
-
-    console.log('YOOOOO', countryCode);
+    setCountry(countryCode);
   }
 
   return (
     <div className="app">
-      <div className="app__header">
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className="app__dropdown">
-          <Select
-            variant="outlined" value={country} onChange={onCountryChange}
-          >
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+      <div className="app__left">
+        <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+            <Select
+              variant="outlined" value={country} onChange={onCountryChange}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases={123} total={2000}/>
+
+          <InfoBox title="Recovered" cases={123} total={3000}/>
+
+          <InfoBox title="Deaths" cases={123} total={4000}/>
+          {/* InfoBoxs title="Coronavirus cases*/}
+          {/* InfoBoxs title="Coronavirus recoveries*/}
+          {/* InfoBoxs */}
+        </div>
+      
+        <Map />
       </div>
       
-      {/* Header */}
-      {/* Title + Select input dropdown field */}
-
-      {/* InfoBoxs */}
-      {/* InfoBoxs */}
-      {/* InfoBoxs */}
-
-      {/* Table */}
-      {/* Graph */}
-
-      {/* Map */}
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          {/* Table */}
+          <h3>Wordwide new cases</h3>
+          {/* Graph */}
+        </CardContent>
+        
+      </Card>
     </div>
   );
 }
